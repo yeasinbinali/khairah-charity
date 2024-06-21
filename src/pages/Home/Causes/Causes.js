c305a54736d82891b272af69f1c909b3be3c6d32
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-22 00:06:54 +0600

    font updated

--- src/pages/Home/Causes/Causes.js
+++ src/pages/Home/Causes/Causes.js
@@ -12,7 +12,7 @@ const Causes = () => {
 
   return (
     <div>
-      <h1 className="text-5xl text-center my-5 font-bold">Causes</h1>
+      <h1 className="text-5xl text-center my-10 font-bold">Causes</h1>
       <div className="mx-auto md:w-4/5 w-full grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-5">
         {causes.map((cause) => (
           <Cause key={cause._id} cause={cause}></Cause>
